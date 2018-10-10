75c1c8f6077949312fb7a65c5db2e806ec493f5a
mfridaa <mayer.friderika@gmail.com>
2018-10-10 08:58:37 +0200

    backend base

---
+++ Backend/src/main/java/com/easyway/backend/Test.java
@@ -0,0 +1,13 @@
+package com.easyway.backend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+	
+	@RequestMapping("test")
+	public String test(){
+		return "Hello Levi a király";
+	}
+}
